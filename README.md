af0c8242ba5f69bb8f0159382be08955db9be1e5
Ömer Faruk Çoban <omer@linxa.com>
2021-01-09 22:07:12 +0300

    create modules

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-admin-server: https://www.baeldung.com/spring-boot-admin
-routing-server: https://github.com/spring-cloud/spring-cloud-gateway
+admin-server: 
+    https://www.baeldung.com/spring-boot-admin
+    https://github.com/codecentric/spring-boot-admin
+
+routing-server:
+    https://spring.io/guides/gs/routing-and-filtering/
+    https://github.com/Netflix/zuul
+
+discovery-server:
+    https://spring.io/guides/gs/service-registration-and-discovery/
+    https://github.com/Netflix/eureka
--- routing-server/src/main/java/com/example/routingserver/RoutingServerApplication.java
+++ routing-server/src/main/java/com/example/routingserver/RoutingServerApplication.java
@@ -1,13 +1,23 @@
 package com.example.routingserver;
 
+import com.example.routingserver.filter.pre.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
+@EnableZuulProxy
+@EnableDiscoveryClient
 @SpringBootApplication
-public class RoutingServerApplication {
+public class RoutingServerApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RoutingServerApplication.class, args);
 	}
 
+	@Bean
+	public SimpleFilter simpleFilter() {
+		return new SimpleFilter();
+	}
 }
