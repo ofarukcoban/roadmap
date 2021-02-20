f9b3b691eeaab47f9fe2412b353ef2a7d26b523e
ofarukcoban <ofarukcoban>
2021-02-20 11:55:31 +0300

    create modules

--- admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
+++ admin-server/src/main/java/com/example/adminserver/AdminServerApplication.java
@@ -3,10 +3,10 @@ package com.example.adminserver;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableAdminServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class AdminServerApplication {
 
--- routing-server/src/main/java/com/example/routingserver/RoutingServerApplication.java
+++ routing-server/src/main/java/com/example/routingserver/RoutingServerApplication.java
@@ -3,12 +3,12 @@ package com.example.routingserver;
 import com.example.routingserver.filter.pre.SimpleFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulServer;
 import org.springframework.context.annotation.Bean;
 
 @EnableZuulServer
-@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class RoutingServerApplication{
 
